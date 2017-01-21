c35ebce11739b672dba523112619d877cbe0434b
Spike Lee <rurouni24@gmail.com>
2017-01-21 23:43:45 +0900

    status bar update
    print current time and connection in status bar

--- pytrader.py
+++ pytrader.py
@@ -20,6 +20,28 @@ class MainWindow(QMainWindow, gui):
 		super().__init__()
 		self.setupUi(self)
 		
+		self.kiwoom = Kiwoom()
+		self.kiwoom.commConnect()
+		
+		# timer
+		self.timer = QTimer(self)
+		self.timer.start(1000)
+		self.timer.timeout.connect(self.timeout)
+		
+	def timeout(self):
+		current_time = QTime.currentTime()
+		text_time = current_time.toString("hh:mm:ss")
+		time_msg = "현재시간 : " + text_time
+		
+		if self.kiwoom.getConnectState() == 1:
+			state_msg = "서버 연결 중"
+		else:
+			state_msg = "서버 끊김"
+			
+		self.statusBar = QStatusBar(self)
+		self.setStatusBar(self.statusBar)
+		self.statusBar.showMessage(state_msg + " | " + time_msg)
+		
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
 	window = MainWindow()

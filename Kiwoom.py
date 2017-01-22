be5226260fd954aeea0a1b3a133a55b1f466d9e4
Spike Lee <rurouni24@gmail.com>
2017-01-22 22:32:53 +0900

    주석 수정
    실행확인

--- Kiwoom.py
+++ Kiwoom.py
@@ -20,7 +20,6 @@ Diff between OpenAPI+ and Kiwoom class
 """
 
 import sys
-import time
 from PyQt5.QAxContainer import QAxWidget
 from PyQt5.QtCore import QEventLoop
 from PyQt5.QtWidgets import QApplication
@@ -31,29 +30,31 @@ class Kiwoom(QAxWidget):
 		self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
 		
 		# variables
-		self.login_event_loop = None
-		self.tr_event_loop = None
-		self.prev_next = None
+		self.login_event_loop   = None
+		self.tr_event_loop      = None
+		self.prev_next          = None
 		
 		# signal & slot
 		self.OnEventConnect.connect(self.onEventConnect)
 		self.OnReceiveTrData.connect(self.onReceiveTrData)
 		self.OnReceiveChejanData.connect(self.onReceiveTradeData)
-		
 	
 	""" ======= METHODS ======= """
-	# set OHLC data form
 	def initOHLCRawData(self):
 		self.ohlc = {'date':[], 'open':[], 'high':[], 'low':[], 'close':[]}
 		
-	# change string to currency format
-	def changeFormat(self, data, percent = 0):
+	def changeFormat(self, str, percent = 0):
+		"""
+		Change string to currency format
+		:param str: 바꿀 문자열
+		:param percent: 0=정수, 1=소수점, 2=백분율
+		"""
 		is_minus = False
 		
-		if data.startswith('-'):
+		if str.startswith('-'):
 			is_minus = True
 			
-		strip_str = data.lstrip('-0')
+		strip_str = str.lstrip('-0')
 		
 		if strip_str == '':
 			if percent == 1:
@@ -79,160 +80,176 @@ class Kiwoom(QAxWidget):
 		self.opw00018_data = {'single': [], 'multi': []}
 	
 	""" ======= OpenAPI+ METHODS ======= """
-	# execute login window
 	def commConnect(self):
+		""" execute login window """
 		self.dynamicCall("CommConnect()")
 		
 		self.login_event_loop = QEventLoop()
 		self.login_event_loop.exec_()
 		
-	# tr을 서버로 송신
-	# nPrevNext : 조회("0"), 연속("2")
 	def commRqData(self, sRQName, sTrCode, nPrevNext, sScreenNo):
+		"""
+		tr을 서버로 송신
+		:param nPrevNext: 0=조회, 2=연속
+		"""
 		self.dynamicCall("CommRqData(QString, QString, int, QString)", sRQName, sTrCode, nPrevNext, sScreenNo)
-		
 		self.tr_event_loop = QEventLoop()
 		self.tr_event_loop.exec_()
 
-	# get user info via sTag
 	def getLoginInfo(self, sTag):
 		ret = self.dynamicCall("GetLoginInfo(QString)", sTag)
 		return ret
 	
-	# send order to server
 	def sendOrder(self, sRQName, sScreenNo, sAccountNo, nOrderType, sItemCode, nQty, nPrice, sBid, sOrgOrderNo):
-		self.dynamicCall("SendOrder(QString, QString, QString, int, QString, int, int, QString, QString)", [sRQName, sScreenNo, sAccountNo, nOrderType, sItemCode, nQty, nPrice, sBid, sOrgOrderNo])
+		ret = self.dynamicCall("SendOrder(QString, QString, QString, int, QString, int, int, QString, QString)",[sRQName, sScreenNo, sAccountNo, nOrderType, sItemCode, nQty, nPrice, sBid, sOrgOrderNo])
+		return ret
 	
-	# transmit input transaction to server
 	def setInputValue(self, sID, sValue):
 		self.dynamicCall("SetInputValue(QString, QString)", sID, sValue)
 	
-	# Tran 데이터, 실시간 데이터, 체결잔고 데이터 반환
 	def commGetData(self, sItemCode, sRealType, sFieldName, nIndex, sInnerFieldName):
+		"""
+		- Transaction 데이터
+		:param sItemCode        : Tr명
+		:param sRealType        : not use
+		:param sFieldName       : 레코드 명
+		:param nIndex           : 반복 인뎃스
+		:param sInnerFieldName  : 아이템 명
+		
+		- 실시간 데이터
+		:param sItemCode        : Key Code
+		:param sRealType        : Real Type
+		:param sFieldName       : Item Index
+		:param nIndex           : not use
+		:param sInnerFieldName  : not use
+		
+		- 체결잔고 데이터
+		:param sItemCode        : 체결구분
+		:param sRealType        : "-1"
+		:param sFieldName       : not use
+		:param nIndex           : Item Index
+		:param sInnerFieldName  : not use
+		"""
 		ret = self.dynamicCall("CommGetData(QString, QString, QString, int, QString)", sItemCode, sRealType, sFieldName, nIndex, sInnerFieldName)
 		return ret.strip()
 	
-	# 화면 내 모든 리얼데이터 요청을 제거
 	def disconnectRealData(self, sScreenNo):
 		pass
 	
-	# 레코드 반복횟수 반환
 	def getRepeatCnt(self, sTrCode, sRecordName):
+		""" 레코드 반복 횟수 리턴"""
 		ret = self.dynamicCall("GetRepeatCnt(QString, QString)", sTrCode, sRecordName)
 		return ret
 	
-	# 시장구분에 따른 종목코드 반환
 	def getCodeListByMarket(self, sMarket):
+		""" 시장구분에 따른 종목코드 리턴"""
 		ret = self.dynamicCall("GetCodeListByMarket(QString)", sMarket)
 		item_code = ret.split(';')
 		return item_code
 	
-	# 현재 접속상태 반환
 	def getConnectState(self):
 		ret = self.dynamicCall("GetConnectState()")
 		return ret
 	
-	# 종목코드의 한글명 반환
 	def getMasterCodeName(self, sItemCode):
+		""" 종목코드 한글명 리턴 """
 		ret = self.dynamicCall("GetMasterCodeName(QString)", sItemCode)
 		return ret
 	
-	# 종목코드의 상장주식수를 반환
 	def getMasterListedStockCnt(self, sItemCode):
+		""" 종목코드의 상장주식 수를 리턴"""
 		pass
 	
-	# 수신 데이터 바환
 	def getCommData(self, sTrCode, sRecordName, nIndex, sItemName):
+		""" 수신데이터 리턴"""
 		pass
 	
-	# 체결잔고 데이터 반환
 	def getTradeData(self, nFid):
+		""" 체결잔고 데이터 리턴"""
 		ret = self.dynamicCall("GetChejanData(int)", nFid)
 		return ret
 	
 	""" ======= OpenAPI+ EVENTS ======= """
-	# 서버통신 후 데이터를 받은 시점을 알려준다
 	def onReceiveTrData(self, sScreenNo, sRQname, sTrCode, sRecordName, sPrevNext):
 		self.prev_next = sPrevNext
-
+		
 		# opt10001 : tran 데이터 저장
-		if sRQname == "opt10081_req":
-			cnt = self.getRepeatCnt(sTrCode, sRQname)
-			
-			for i in range(cnt):
-				date    = self.commGetData(sTrCode, "", sRQname, i, "일자")
-				open    = self.commGetData(sTrCode, "", sRQname, i, "시가")
-				high    = self.commGetData(sTrCode, "", sRQname, i, "고가")
-				low     = self.commGetData(sTrCode, "", sRQname, i, "저가")
-				close   = self.commGetData(sTrCode, "", sRQname, i, "현재가")
-				
-				self.ohlc['date'].append(date)
-				self.ohlc['open'].append(int(open))
-				self.ohlc['high'].append(int(high))
-				self.ohlc['low'].append(int(low))
-				self.ohlc['close'].append(int(close))
-		# opw00001 : 예수금 정보
-		elif sRQname == "opw00001_req":
-			estimated_day2_deposit = self.commGetData(sTrCode, "", sRQname, 0, "d+2추정예수금")
-			estimated_day2_deposit = self.changeFormat(estimated_day2_deposit)
-			self.opw00001_data = estimated_day2_deposit
-		# opw00018 : 계좌평가잔고내역 (한 번의 TR요청으로 최대 20개 보유 종목 가져옴)
-		elif sRQname == "opw00018_req":
-			# single data
-			single_data = []
-			
-			total_purchase_price = self.commGetData(sTrCode, "", sRQname, 0, "총매입금액")
-			single_data.append(self.changeFormat(total_purchase_price))
-			
-			total_eval_price = self.commGetData(sTrCode, "", sRQname, 0, "총평가금액")
-			single_data.append(self.changeFormat(total_eval_price))
-			
-			total_eval_profit_loss_price = self.commGetData(sTrCode, "", sRQname, 0, "총평가손익금액")
-			single_data.append(self.changeFormat(total_eval_profit_loss_price))
-			
-			total_profit_rate = self.commGetData(sTrCode, "", sRQname, 0, "총수익률(%)")
-			single_data.append(self.changeFormat(total_profit_rate, 1))
-			
-			estimated_deposit = self.commGetData(sTrCode, "", sRQname, 0, "추정예탁자산")
-			single_data.append(self.changeFormat(estimated_deposit))
-			
-			self.opw00018_data['single'] = single_data
-			
-			# multi data
-			cnt = self.getRepeatCnt(sTrCode, sRQname)
-			for i in range (cnt):
-				multi_data = []
-				
-				item_name = self.commGetData(sTrCode, "", sRQname, i, "종목명")
-				multi_data.append(item_name)
-				
-				quantity = self.commGetData(sTrCode, "", sRQname, i, "보유수량")
-				multi_data.append(self.changeFormat(quantity))
-				
-				purchase_price = self.commGetData(sTrCode, "", sRQname, i, "매입가")
-				multi_data.append(self.changeFormat(purchase_price))
-				
-				current_price = self.commGetData(str, "", sRQname, i, "현재가")
-				multi_data.append(self.changeFormat(current_price))
-				
-				eval_profit_loss_price = self.commGetData(sTrCode, "", sRQname, i, "평가손익")
-				multi_data.append(self.changeFormat(eval_profit_loss_price))
-				
-				profit_rate = self.commGetData(sTrCode, "", sRQname, i, "수익률(%)")
-				multi_data.append(self.changeFormat(profit_rate, 2))
-				
-				self.opw00018_data['multi'].append(multi_data)
-		self.tr_event_loop.exit()
+		# if sRQname == "opt10001_req":
+		# 	cnt = self.getRepeatCnt(sTrCode, sRQname)
+		#
+		# 	for i in range(cnt):
+		# 		date    = self.commGetData(sTrCode, "", sRQname, i, "일자")
+		# 		open    = self.commGetData(sTrCode, "", sRQname, i, "시가")
+		# 		high    = self.commGetData(sTrCode, "", sRQname, i, "고가")
+		# 		low     = self.commGetData(sTrCode, "", sRQname, i, "저가")
+		# 		close   = self.commGetData(sTrCode, "", sRQname, i, "현재가")
+		#
+		# 		self.ohlc['date'].append(date)
+		# 		self.ohlc['open'].append(int(open))
+		# 		self.ohlc['high'].append(int(high))
+		# 		self.ohlc['low'].append(int(low))
+		# 		self.ohlc['close'].append(int(close))
+		# # opw00001 : 예수금 정보
+		# elif sRQname == "opw00001_req":
+		# 	estimated_day2_deposit = self.commGetData(sTrCode, "", sRQname, 0, "d+2추정예수금")
+		# 	estimated_day2_deposit = self.changeFormat(estimated_day2_deposit)
+		# 	self.opw00001_data = estimated_day2_deposit
+		# # opw00018 : 계좌평가잔고내역 (한 번의 TR요청으로 최대 20개 보유 종목 가져옴)
+		# elif sRQname == "opw00018_req":
+		# 	# single data
+		# 	single_data = []
+		#
+		# 	total_purchase_price = self.commGetData(sTrCode, "", sRQname, 0, "총매입금액")
+		# 	single_data.append(self.changeFormat(total_purchase_price))
+		#
+		# 	total_eval_price = self.commGetData(sTrCode, "", sRQname, 0, "총평가금액")
+		# 	single_data.append(self.changeFormat(total_eval_price))
+		#
+		# 	total_eval_profit_loss_price = self.commGetData(sTrCode, "", sRQname, 0, "총평가손익금액")
+		# 	single_data.append(self.changeFormat(total_eval_profit_loss_price))
+		#
+		# 	total_profit_rate = self.commGetData(sTrCode, "", sRQname, 0, "총수익률(%)")
+		# 	single_data.append(self.changeFormat(total_profit_rate, 1))
+		#
+		# 	estimated_deposit = self.commGetData(sTrCode, "", sRQname, 0, "추정예탁자산")
+		# 	single_data.append(self.changeFormat(estimated_deposit))
+		#
+		# 	self.opw00018_data['single'] = single_data
+		#
+		# 	# multi data
+		# 	cnt = self.getRepeatCnt(sTrCode, sRQname)
+		# 	for i in range (cnt):
+		# 		multi_data = []
+		#
+		# 		item_name = self.commGetData(sTrCode, "", sRQname, i, "종목명")
+		# 		multi_data.append(item_name)
+		#
+		# 		quantity = self.commGetData(sTrCode, "", sRQname, i, "보유수량")
+		# 		multi_data.append(self.changeFormat(quantity))
+		#
+		# 		purchase_price = self.commGetData(sTrCode, "", sRQname, i, "매입가")
+		# 		multi_data.append(self.changeFormat(purchase_price))
+		#
+		# 		current_price = self.commGetData(str, "", sRQname, i, "현재가")
+		# 		multi_data.append(self.changeFormat(current_price))
+		#
+		# 		eval_profit_loss_price = self.commGetData(sTrCode, "", sRQname, i, "평가손익")
+		# 		multi_data.append(self.changeFormat(eval_profit_loss_price))
+		#
+		# 		profit_rate = self.commGetData(sTrCode, "", sRQname, i, "수익률(%)")
+		# 		multi_data.append(self.changeFormat(profit_rate, 2))
+		#
+		# 		self.opw00018_data['multi'].append(multi_data)
+		print("11111111111111111")
+		# self.tr_event_loop.exit()
+		print("22222222222222222")
 
-	# 실시간 데이터를 받은 시점을 알려준다
 	def onReceiveRealData(self, sItemCode):
 		pass
 	
-	# 서버통신 후 메시지를 받은 시점을 알려준다
 	def onReceiveMsg(self, sScrNo, sRQname, sTrCode, sMsg):
 		pass
 	
-	# 체결데이터를 받은 시점을 알려준다
 	def onReceiveTradeData(self, sTradeType, nItemCnt, sFidList):
 		print("sTradeType : ", sTradeType)
 		print(self.getTradeData(9203))
@@ -240,7 +257,6 @@ class Kiwoom(QAxWidget):
 		print(self.getTradeData(900))
 		print(self.getTradeData(909))
 	
-	# server connection event
 	def onEventConnect(self, nErrCode):
 		if nErrCode == 0:
 			print("connected")
@@ -249,9 +265,15 @@ class Kiwoom(QAxWidget):
 			
 		self.login_event_loop.exit()
 		
-	# 조건검색 실시간 편입, 이탈 종목을 받을 시점을 알려준다
-	# sType                 : 편입("I"), 이탈("D")
 	def onReceiveCondition(self, sItemCode, sType, sConditionName, sConditionIndex):
+		"""
+		조건검색 실시간 편입, 이탈 종목을 받을 시점을 알려준다
+		:param sItemCode:
+		:param sType: 'I' = 편입, 'D' = 이탈
+		:param sConditionName:
+		:param sConditionIndex:
+		:return:
+		"""
 		pass
 	
 if __name__ == "__main__":
@@ -260,5 +282,5 @@ if __name__ == "__main__":
 	kiwoom = Kiwoom()
 	kiwoom.commConnect()
 	kiwoom.initOHLCRawData()
-	
 	kiwoom.initOpw00018data()
+	app.exec_()
--- pytrader.py
+++ pytrader.py
@@ -5,8 +5,7 @@ Date        : 2017.01.21
 
 Load UI and handle events
 """
-
-import sys
+import time
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
@@ -22,23 +21,33 @@ class MainWindow(QMainWindow, gui):
 		self.kiwoom = Kiwoom()
 		self.kiwoom.commConnect()
 		
+		# get user account ID/PW
+		self.account = []
+		with open("account.txt", 'r') as f:
+			self.account = f.readlines()
+		
 		# get account info
 		account_cnt  = int(self.kiwoom.getLoginInfo("ACCOUNT_CNT"))
 		account_no   = self.kiwoom.getLoginInfo("ACCNO")
 		account_list = account_no.split(';')[0:account_cnt]
 		
-		# timer
-		self.timer = QTimer(self)
-		self.timer.start(1000)
-		self.timer.timeout.connect(self.timeout)
+		# timer in status bar
+		self.timer_status_bar = QTimer(self)
+		self.timer_status_bar.start(1000)
+		self.timer_status_bar.timeout.connect(self.timeoutStatusBar)
+		
+		# timer in table widgets
+		self.timer_table = QTimer(self)
+		self.timer_table.start(10000)
+		self.timer_table.timeout.connect(self.timeoutBalanceTable)
 		
 		# Window forms
 		self.qtOrder_comboBox_account.addItems(account_list)
 		self.qtOrder_lineEdit_item.textChanged.connect(self.itemCodeChanged)
 		self.qtOrder_pushButton_sendOrder.clicked.connect(self.sendOrder)
+		self.qtTrade_pushButton_check.clicked.connect(self.checkBalance)
 		
-	# 시간 및 접속 정보 출력
-	def timeout(self):
+	def timeoutStatusBar(self):
 		current_time = QTime.currentTime()
 		text_time = current_time.toString("hh:mm:ss")
 		time_msg = "현재시간 : " + text_time
@@ -51,26 +60,72 @@ class MainWindow(QMainWindow, gui):
 		self.statusBar = QStatusBar(self)
 		self.setStatusBar(self.statusBar)
 		self.statusBar.showMessage(state_msg + " | " + time_msg)
+	
+	def timeoutBalanceTable(self):
+		if self.qtTrade_checkBox_realtime.isChecked() == True:
+			self.checkBalance()
 		
-	# 종목명 한글출력
 	def itemCodeChanged(self):
+		""" print item code by hangul """
 		code = self.qtOrder_lineEdit_item.text()
 		name = self.kiwoom.getMasterCodeName(code)
 		self.qtOrder_lineEdit_code.setText(name)
 		
-	# 수동주문
 	def sendOrder(self):
 		order_type_lookup = {"신규매수":1, "신규매도":2, "매수취소":3, "매도취소":4}
 		bid_lookup        = {"지정가":"00", "시장가":"03"}
-		
+
 		account     = self.qtOrder_comboBox_account.currentText()
 		order_type  = self.qtOrder_comboBox_order.currentText()
 		code        = self.qtOrder_lineEdit_item.text()
 		bid         = self.qtOrder_comboBox_type.currentText()
 		num         = self.qtOrder_spinBox_qty.value()
 		price       = self.qtOrder_spinBox_price.value()
+
+		e = self.kiwoom.sendOrder("sendOrder_req", "0101", account, order_type_lookup[order_type], code, num, price, bid_lookup[bid], "")
+		print(e)
 		
-		self.kiwoom.sendOrder("sendOrder_req", "0101", account, order_type_lookup[order_type], code, num, price, bid_lookup[bid], "")
+	def checkBalance(self):
+		self.kiwoom.initOpw00018data()
+		
+		# request opw00001
+		self.kiwoom.setInputValue("계좌번호", "8087071611")
+		self.kiwoom.setInputValue("비밀번호", self.account[1])
+		self.kiwoom.commRqData("opw00001_req", "opw00001", 0, "2000")
+		
+		# request opw00018
+		self.kiwoom.setInputValue("계좌번호", "8087071611")
+		self.kiwoom.setInputValue("비밀번호", self.account[1])
+		self.kiwoom.commRqData("opw00018_req", "opw00018", 0, "2000")
+		
+		while self.kiwoom.prev_next == '2':
+			time.sleep(0.2)
+			self.kiwoom.setInputValue("계좌번호", "8087071611")
+			self.kiwoom.setInputValue("비밀번호", self.account[1])
+			self.kiwoom.commRqData("opw00018_req", "opw00018", 0, "2000")
+		
+		# print balance at table
+		item = QTableWidgetItem(self.kiwoom.opw00001_data)
+		item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+		self.qtTrade_tableWidget.setItem(0, 0, item)
+		
+		for i in range(1, 6):
+			item = QTableWidgetItem(self.kiwoom.opw00018_data['single'][i-1])
+			item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+			self.qtTrade_tableWidget2.setItem(0, i, item)
+		self.qtTrade_tableWidget.resizeRowsToContents()
+		
+		# print own stock items at table
+		item_cnt = len(self.kiwoom.opw00018_data['multi'])
+		self.qtTrade_tableWidget2.setRowCount(item_cnt)
+		
+		for i in range(item_cnt):
+			row = self.kiwoom.opw00018_data['multi'][i]
+			for j in range(len(row)):
+				item = QTableWidgetItem(row[i])
+				item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+				self.qtTrade_tableWidget2.setItem(i, j, item)
+		self.qtTrade_tableWidget2.resizeRowsToContents()
 		
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
